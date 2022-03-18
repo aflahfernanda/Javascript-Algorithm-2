27583dab8e064eb73302718d2d4f7001a45d71e7
Aflah Fernanda <101084909+aflahfernanda@users.noreply.github.com>
2022-03-18 14:45:42 +0700

    Update task 2.js

--- task 2.js
+++ task 2.js
@@ -1,65 +1,36 @@
-//dengan callback null
-// const getMonth=(callback)=>{
-//     setTimeout(()=>{
-//         let error=false
-//         let month=['januari','februari','maret','april','mei','juni','juli',
-//                     'agustus','september','oktober','november','desember']
-//         if(!error){
-//             callback(null,month)
-//         }else{
-//             callback(new Error('sorry data not found'),[])
-//         }
-//     },3000)
-// }
-// const showMonth=(month,isError)=>{
-//     let monthMap
-//     setTimeout(()=>{
-//         if(isError){
-//             new Error('sorry data not found')
-//         }else  {
-//             monthMap=month.map(function(e){
-//                 return e
-//             })
-//             monthMapJoin=monthMap.join('\n')
-//             console.log(monthMapJoin)}
-//     },1000)
-// }
+const getMonth = (callback) => {
+  setTimeout(() => {
+    let error = false;
+    let month = [
+      "januari",
+      "februari",
+      "maret",
+      "april",
+      "mei",
+      "juni",
+      "juli",
+      "agustus",
+      "september",
+      "oktober",
+      "november",
+      "desember",
+    ];
+    if (!error) {
+      callback(null, month);
+    } else {
+      callback(new Error("sorry data not found"), []);
+    }
+  }, 3000);
+};
 
-// getMonth((isError,hasilMonth) => {
-//     showMonth(hasilMonth)
-// })
-
-//-------------------------------------------------------------------------------------------------------------------------------
-//tanpa callback(null)
-
-
-
-
-const getMonth=(callback)=>{
-    setTimeout(()=>{
-        let error=false
-        let month=['januari','februari','maret','april','mei','juni','juli',
-                    'agustus','september','oktober','november','desember']
-        if(!error){
-            callback(month)
-        }else{
-            callback(new Error('sorry data not found'),[])
-        }
-    },3000)
-}
-const showMonth=(month,isError)=>{
-    let monthMap
-    setTimeout(()=>{
-        if(isError===Error){
-            new Error('sorry data not found')
-        }else  {
-            monthMap=month.map(function(e){
-                return e
-            })
-            monthMapJoin=monthMap.join('\n')
-            console.log(monthMapJoin)}
-    },1000)
+function showMonth(dataError, isMonth) {
+  setTimeout(() => {
+    if (dataError) {
+      return console.log(new Error("sorry data not found"));
+    } else {
+      return isMonth.map((month) => console.log(month));
+    }
+  }, 1000);
 }
 
-getMonth(showMonth)
-
+getMonth(showMonth);
